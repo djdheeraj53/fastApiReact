0dc2aece1736ac44715e329daa80dd734fd6bb04
Dheeraj Kumar <dheerajkumar@Dheerajs-MacBook-Pro.local>
2025-02-11 19:51:45 +0530

    start application fastapi

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+
+def index():
+    return {
+        'data': {
+            'name': 'dheeraj'
+        }
+    }
+
+@app.get('/about')
+def about():
+    return {'data': "about page"}
