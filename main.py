226d7553b3eeb4533d2273f5c22a3489ee8b9056
Dheeraj Kumar <dheerajkumar@Dheerajs-MacBook-Pro.local>
2025-02-11 23:20:07 +0530

    get api

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
-
 def index():
     return {
         'data': {
@@ -11,6 +12,28 @@ def index():
         }
     }
 
-@app.get('/about')
-def about():
-    return {'data': "about page"}
+@app.get('/blog')
+def about(limit=10, publish: bool = True, sort: Optional[str] = None):
+    if publish:
+        return publish
+    else:
+        return {'data': f"blog list {limit} {publish}"}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return id
+
+class Blog(BaseModel):
+    pass
+
+@app.post('/blog')
+def create_blog():
+    return {'data': 'blog created'}
